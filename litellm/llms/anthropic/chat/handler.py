a6b750c05d31588f987151a6362b34b4063887b6
FBIKKIBF <fbikkibf@gmail.com>
2026-03-19 16:53:22 +0800

    Full fix with tests

--- litellm/llms/anthropic/chat/handler.py
+++ litellm/llms/anthropic/chat/handler.py
@@ -1153,8 +1153,8 @@ class ModelResponseIterator:
 
         if str_line.startswith("data:"):
             chunk_str = str_line[5:].strip()
-            # Models like Deepseek might return "data: [DONE]" here which is not a valid JSON input.
-            # We can just ignore these chunks.
+            # Models like Deepseek might return "data: [DONE]" here which is not a
+            # valid JSON input. We can just ignore these chunks.
             try:
                 data_json = json.loads(chunk_str)
                 return self.chunk_parser(chunk=data_json)
